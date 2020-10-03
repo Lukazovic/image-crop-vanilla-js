5f2842d6d5e73bdadc192354cb414f243f4d282b
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-10-03 19:41:04 -0300

    Crop image

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Selection tool
 
-const slection = document.getElementById('selection-tool');
+const selection = document.getElementById('selection-tool');
 let startX,
   startY,
   relativeStartX,
@@ -54,12 +54,12 @@ const events = {
     endY = event.clientY;
 
     if (startSelection) {
-      slection.style.display = 'initial';
-      slection.style.top = `${startY}px`;
-      slection.style.left = `${startX}px`;
+      selection.style.display = 'initial';
+      selection.style.top = `${startY}px`;
+      selection.style.left = `${startX}px`;
 
-      slection.style.width = `${endX - startX}px`;
-      slection.style.height = `${endY - startY}px`;
+      selection.style.width = `${endX - startX}px`;
+      selection.style.height = `${endY - startY}px`;
     }
   },
   mouseup() {
@@ -69,6 +69,9 @@ const events = {
 
     relativeEndX = layerX;
     relativeEndY = layerY;
+
+    // mostrar o botão de corte
+    cropButton.style.display = 'initial';
   },
 };
 
@@ -92,5 +95,56 @@ image.onload = function () {
   // desenhar a imagem no canvas
   ctx.drawImage(image, 0, 0);
 
+  photoPreview.src = canvas.toDataURL();
+};
+
+// Cortar imagem
+const cropButton = document.getElementById('crop-image');
+cropButton.onclick = () => {
+  const { width: imgWidth, height: imgHeight } = image;
+  const { width: previewWidth, height: previewHeight } = photoPreview;
+
+  const [widthFactor, heightFactor] = [
+    +(imgWidth / previewWidth),
+    +(imgHeight / previewHeight),
+  ];
+
+  const [selectionWidth, selectionHeight] = [
+    +selection.style.width.replace('px', ''),
+    +selection.style.height.replace('px', ''),
+  ];
+
+  const [croppedWidth, croppedHeight] = [
+    +(selectionWidth * widthFactor),
+    +(selectionHeight * heightFactor),
+  ];
+
+  const [actualX, actualY] = [
+    +(relativeStartX * widthFactor),
+    +(relativeStartY * heightFactor),
+  ];
+
+  // pegar do ctx as regiões de corte da imagem
+  const croppedImage = ctx.getImageData(
+    actualX,
+    actualY,
+    croppedWidth,
+    croppedHeight
+  );
+
+  // limpar o ctx
+  ctx.clearRect(0, 0, ctx.width, ctx.height);
+
+  // ajuste de proporções
+  image.width = canvas.width = croppedWidth;
+  image.height = canvas.height = croppedHeight;
+
+  // adicionar a imagem cortada ao contexto do canvas
+  ctx.putImageData(croppedImage, 0, 0);
+
+  // esconder a ferramenta de seleção
+  selection.style.display = 'none';
+
+  // atualizar o preview da imagem
   photoPreview.src = canvas.toDataURL();
 };
